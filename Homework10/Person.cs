6903c6ffb399cdf69c81b7d46f4b21d5b2dace09
Rayle1gh <DarkMaxim003@gmail.com>
2020-02-10 15:57:03 +0300

    DZ 10

---
+++ Homework10/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework10
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int AgeIn4Years { get { return Age + 4; } }
+        public string Description { get { return $"{nameof(Name)}: {Name}, age in 4 years: {AgeIn4Years}."; } }
+    }
+}
---
+++ Homework10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Enter name {i}: ");
+                string name = Console.ReadLine();
+
+            inputAge:
+                Console.Write($"Enter age {i}: ");
+
+                if (int.TryParse(Console.ReadLine(), out int age))
+                {
+                    if (age > 0)
+                    {
+                        var person = new Person
+                        {
+                            Name = name,
+                            Age = age
+                        };
+                        people.Add(person);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Возраст должен быть > 0");
+                        goto inputAge;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат возраста!");
+                    goto inputAge;
+                }
+            }
+            people.ForEach((p) =>
+            {
+                Console.WriteLine(p.Description);
+            });
+            Console.ReadKey();
+        }
+    }
+}
