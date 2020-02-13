34fb79b0590888940a25344cbd994140f8aefd9a
Rayle1gh <DarkMaxim003@gmail.com>
2020-02-13 18:49:02 +0300

    DZ 11. small fix

--- Homework11/ReminderItem.cs
+++ Homework11/ReminderItem.cs
@@ -4,12 +4,6 @@ namespace Homework11
 {
     public class ReminderItem : IReminderItem
     {
-        public ReminderItem(DateTimeOffset date, string message)
-        {
-            AlarmDate = date;
-            AlarmMessage = message;
-        }
-
         public DateTimeOffset AlarmDate { get; set; }
         public string AlarmMessage { get; set; }
         public TimeSpan TimeToAlarm { get { return DateTimeOffset.Now - AlarmDate; } }
@@ -26,6 +20,11 @@ namespace Homework11
         {
             Console.WriteLine("AlarmDate: {0}\nAlarmMessage: {1}\nTimeToAlarm: {2:%d} days {2:%h} hours {2:%m} minutes {2:%s} seconds\nIsOutdated: {3}",
                  AlarmDate.DateTime, AlarmMessage, TimeToAlarm, IsOutdated);
+        }
+        public ReminderItem(DateTimeOffset date, string message)
+        {
+            AlarmDate = date;
+            AlarmMessage = message;
         }
     }
 }
