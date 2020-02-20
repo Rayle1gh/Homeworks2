df2a71189d7212d90a704b6dc11c0560e5578443
Rayle1gh <DarkMaxim003@gmail.com>
2020-02-20 18:52:09 +0300

    DZ 14

---
+++ Homework14/FLW.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Homework13
+{
+    class FileLogWriter : AbstractLogWriter, IDisposable
+    {
+        private StreamWriter _writer;
+        public FileLogWriter(string logFile = "log.txt")
+        {
+            _writer = new StreamWriter(File.Open(logFile, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read));
+            _writer.BaseStream.Seek(0, SeekOrigin.End);
+        }
+        public override void LogWriting(string message)
+        {
+            _writer.WriteLine(message);
+            _writer.Flush();
+        }
+        public void Dispose()
+        {
+            _writer?.Dispose();
+        }
+    }
+}
