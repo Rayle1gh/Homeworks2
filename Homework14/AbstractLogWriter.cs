c867ac5a2459af5c8228732cd70ed1c92a5f2e2f
Rayle1gh <DarkMaxim003@gmail.com>
2020-02-21 22:58:50 +0300

    DZ 14 FIX

--- Homework14/AbstractLogWriter.cs
+++ Homework14/AbstractLogWriter.cs
@@ -8,18 +8,18 @@ namespace Homework13
     {
         public enum LogLevel { Info, Error, Warning }
 
-        private const string _logMessage = "{0:yyyy-MM-ddThh:mm:ss:ffff}\t{1}\t{2}";
+        private const string _logMessageTemplate = "{0:yyyy-MM-ddThh:mm:ss:ffff}\t{1}\t{2}";
         public void LogError(string message)
         {
-            LogWriting(String.Format(_logMessage, DateTime.Now, LogLevel.Error, message));
+            LogWriting(String.Format(_logMessageTemplate, DateTime.Now, LogLevel.Error, message));
         }
         public void LogInfo(string message)
         {
-            LogWriting(String.Format(_logMessage, DateTime.Now, LogLevel.Info, message));
+            LogWriting(String.Format(_logMessageTemplate, DateTime.Now, LogLevel.Info, message));
         }
         public void LogWarning(string message)
         {
-            LogWriting(String.Format(_logMessage, DateTime.Now, LogLevel.Warning, message));
+            LogWriting(String.Format(_logMessageTemplate, DateTime.Now, LogLevel.Warning, message));
         }
         public abstract void LogWriting(string message);
     }
